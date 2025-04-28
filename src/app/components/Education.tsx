84834d1f08280a957b93272b9bfb38bed584cc9f
xickysahab <jainaagam908@gmail.com>
2025-04-28 05:40:48 +0530

    headings changes

--- src/app/components/Education.tsx
+++ src/app/components/Education.tsx
@@ -8,7 +8,7 @@ const Education: React.FC = () => {
       <div style={{ marginBottom: '20px', padding: '20px', border: '1px solid #ddd', borderRadius: '8px', display: 'flex', alignItems: 'center' }}>
         <Image src="/IIT madras.png" alt="IIT Madras Logo" width={50} height={50} style={{ marginRight: '15px', objectFit: 'contain' }} />
         <div>
-          <h3 style={{ marginTop: '0', marginBottom: '5px', color: '#007bff' }}>Indian Institute of Technology Madras</h3>
+          <h3 style={{ marginTop: '0', marginBottom: '5px', color: 'var(--primary-color)' }}>Indian Institute of Technology Madras</h3>
           <p style={{ margin: '0 0 10px 0', fontStyle: 'italic' }}>First Year AI Engineering Student</p>
             {/* Add degree, dates, relevant coursework, etc. */}
           </div>
@@ -16,7 +16,7 @@ const Education: React.FC = () => {
       <div style={{ marginBottom: '20px', padding: '20px', border: '1px solid #ddd', borderRadius: '8px', display: 'flex', alignItems: 'center' }}>
         <Image src="/Bvm logo.jpg" alt="Bhai Parmanad Vidya Mandir Logo" width={50} height={50} style={{ marginRight: '15px', objectFit: 'contain' }} />
         <div>
-          <h3 style={{ marginTop: '0', marginBottom: '5px', color: '#007bff' }}>Bhai Parmanad Vidya Mandir</h3>
+          <h3 style={{ marginTop: '0', marginBottom: '5px', color: 'var(--primary-color)' }}>Bhai Parmanad Vidya Mandir</h3>
           <p style={{ margin: '0 0 10px 0', fontStyle: 'italic' }}>Completed Schooling from Science Side</p>
           {/* Add degree, dates, relevant coursework, etc. */}
         </div>
